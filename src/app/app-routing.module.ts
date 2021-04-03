3e9486915526ef43dfc479d023c5f5c0341e1cee
javierfm <jadosfer@hotmail.com>
2021-04-02 22:30:55 -0300

    hago el routing de los componentes y los conecto con la nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContractsComponent } from './contracts/contracts.component';
+import { PresentationComponent } from './presentation/presentation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contracts', component: ContractsComponent },
+  { path: 'presentation', component: PresentationComponent },
+  { path: '', redirectTo: 'contracts', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
